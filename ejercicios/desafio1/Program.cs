a73eb706ffa16f7c4a01fedcb4ce7e2d9e4e6394
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 17:35:49 -0300

    ejercicios

---
+++ ejercicios/desafio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Telefono T1 = new Telefono("iphone", " XR PRO ");
+          //  Console.WriteLine("el  modelo es: " + T1.getModelo());
+          //  Console.WriteLine("la marca es: " + T1.getMarca());
+            T1.setNumeroTelefonico("3585986788");
+           // Console.WriteLine("el numero de telefono es " + T1.getNumeroTelefonico());
+            T1.setCodigoOperador(1);
+            //Console.WriteLine("el codigo de operador es: " + T1.getCodigoOperador());
+            Console.WriteLine(T1.Llamar());
+            Console.WriteLine( T1.Llamar("tomy"));
+            Console.WriteLine("el modelo del telefono es: " + T1.getModelo() + "la marca del telefono es: " + T1.getMarca() + "el numero del telefono es: " + T1.getNumeroTelefonico() + "el numero de operador es: " + T1.getCodigoOperador());
+            
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ ejercicios/desafio1/Telefono.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    class Telefono
+    {
+        private string Modelo;
+        private string Marca;
+        private string NumeroTelefonico;
+        private int CodigoOperador;
+        
+       //propiedades.
+        public string getModelo()
+        {
+            return Modelo;
+        }
+        public string getMarca()
+        {
+            return Marca;
+        }
+        public string getNumeroTelefonico()
+        {
+            return NumeroTelefonico;
+        }
+        public void setNumeroTelefonico(string numero)
+        {
+            NumeroTelefonico = numero;
+        }
+        public int  getCodigoOperador()
+        {
+            return CodigoOperador;  
+        }
+        public void setCodigoOperador(int codigo)
+        {
+            if(codigo > 0 && codigo < 4)
+            {
+                CodigoOperador = codigo;
+            }
+            
+        }
+        //constructor.
+
+        public Telefono(string Marca, string Modelo)
+        {
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+        }
+        //destructor.
+        public Telefono() { }
+
+        //metodos.
+        public string Llamar()
+        {
+            return "realizando llamada...";
+        }
+        //sobrecarga de metodo.
+        public string Llamar(string contacto)
+        {
+            return "llamando a " + contacto;
+        }
+    }
+}
